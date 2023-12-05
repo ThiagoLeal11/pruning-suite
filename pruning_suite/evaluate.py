61aec40d87a5a90bc1e31b89d9ad1578d34295a3
Thiago L. Pozati <thiagoleal11@gmail.com>
2023-12-04 22:17:22 -0300

    Feat: evaluate.top_accuracy now return the losses as well

--- pruning_suite/evaluate.py
+++ pruning_suite/evaluate.py
@@ -1,9 +1,11 @@
 import torch
 from timm.utils import accuracy, AverageMeter
+from torch import nn
 
 
 def top_accuracy(model, loader, top_k=(1,), device='cpu'):
     """Computes the accuracy over the k top predictions for the specified values of k"""
+    losses = AverageMeter()
     tops = [AverageMeter() for _ in range(len(top_k))]
 
     model.eval()
@@ -12,8 +14,12 @@ def top_accuracy(model, loader, top_k=(1,), device='cpu'):
             inputs, target = inputs.to(device), target.to(device)
             output = model(inputs)
 
+            criterion = nn.CrossEntropyLoss().to(device)
+            loss = criterion(output, target)
+            losses.update(loss.item(), inputs.size(0))
+
             accs = accuracy(output, target, topk=top_k)
             for i, acc in enumerate(accs):
                 tops[i].update(acc.item(), inputs.size(0))
 
-    return [top.avg for top in tops]
+    return losses.avg, [top.avg for top in tops]
